4e4653574d06f875d81aa2ab4f4bbab5b980d513
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-15 15:43:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,14 @@ def summon_captain_planet(array)
 end
 
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  new_planet = []
+  array.collect do |name|
+    new_planet << name
+    return true if name.length > 4
+    return false if name.length < 4
+  end
 end
 
 def find_the_cheese# code an argument here
