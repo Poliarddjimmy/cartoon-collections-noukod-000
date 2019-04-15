59cde53a4653404efb2c73e72718b51adaf77355
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-15 15:46:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,13 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  
+  array.any? do |word|
+    if word.length > 4
+      return true
+    else
+      return false
+    end
+  end
 end
 
 def find_the_cheese# code an argument here
